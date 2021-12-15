0710270fc0b41d6438af330c69f9fd5f3acfcf70
alexjove <alexdjove@gmail.com>
2021-12-15 19:29:21 +0100

    base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Ya he realizado todos los cambios necesarios</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager</h1>
+      <p>Pending Tasks: 5</p>
+      <form>
+        <input type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        <li>Do dishes</li>
+        <li>Buy milk</li>
+      </ul>
     </div>
   );
 }
