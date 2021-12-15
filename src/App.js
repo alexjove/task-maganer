1c3d2c004c712d229d162386f5853d5aae9ca9bb
alexjove <alexdjove@gmail.com>
2021-12-15 19:33:24 +0100

    variable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 function App() {
+  
+  const pendingTasks = 7;
+  const tasks = ["Do dishes", "Buy milk", "Walk the dog"];
+
   return (
     <div>
       <h1>Task Manager</h1>
-      <p>Pending Tasks: 5</p>
+      <p>Pending Tasks: {pendingTasks}</p>
       <form>
         <input type="text" />
         <button type="submit">Add</button>
       </form>
       <ul>
-        <li>Do dishes</li>
-        <li>Buy milk</li>
+        {tasks.map(task =>{
+          return <li>{task}</li>
+        })}
       </ul>
     </div>
   );
