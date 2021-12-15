dfe4b05de0e221cdafc1a27a57dea984ec48f00a
alexjove <alexdjove@gmail.com>
2021-12-15 21:41:16 +0100

    with all focus on pendingTasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -6,39 +6,47 @@ function App() {
   const [title, setTitle] = useState("")
   const [tasks, setTasks] = useState([])
   const pendingTasks = tasks.length;
+  const inputRef = useRef();
+  const listRef = useRef();
 
   useEffect(() => {
     document.title = pendingTasks + "task remaining";
       }, [pendingTasks]);
 
-      useEffect(() => {
-        if (pendingTasks === 0) {
-          alert("Congrats");
-      }}, [pendingTasks]);
+      //Mensaje cuando has acabado todas las tareas
+  //useEffect(() => {
+   //     if (pendingTasks === 0) {
+   //       alert("Congrats");
+   //   }}, [pendingTasks]); 
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div>
       <h1>Task Manager</h1>
-      <p>Pending Tasks: {pendingTasks}</p>
-      <div>
-        <input 
-        value={title} 
-        onChange={(event) => { 
-        setTitle(event.target.value);
-      }}
-        type="text" 
-        />
-
-        <button
-        onClick={() => {
+      <p>Pending tasks: {pendingTasks}</p>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
           setTasks([title, ...tasks]);
           setTitle("");
+          inputRef.current.focus();
+          listRef.current.scrollTop = 0;
         }}
-        >
-          Add task</button>
-
-      </div>
-      <ul className="task-list">
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul ref={listRef} className="task-list">
         {tasks.map((task) => {
           return (
             <li
